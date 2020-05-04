f319554023e66375c95b1538de9cd112b74eb94e
Peter Kilfeather <peterkilfeather@yarra.local>
2020-05-04 18:37:48 +0100

    Some QC of the RPKM data

--- VTA vs SNc analysis code.R
+++ VTA vs SNc analysis code.R
@@ -1,21 +1,119 @@
 ## Now analysing VTA vs SNc neurons (hence SV)
 library(DESeq2)
-library(tidyverse)
 library(tximeta)
 
-setwd("Downloads/billy/")
+library(dplyr)
+library(tidyr)
+library(readr)
+library(tibble)
+library(ggplot2)
+
+getwd()
 
 ctsSV <- as.matrix(read.delim("Human_LCM_SNc_VTA_.txt", sep = "", row.names = "Gene_ID")) #count matrix
 
-MetaSV <- file.path("Meta_LCM_hum_VTA_SNC.csv") # Metadata file
-coldataSV <- read.csv(MetaSV, row.names = 1)
-coldataSV <- coldataSV[,c("Cell_Type", "tissue")] #[] is for where the data is -- (_rows, _columns)
+#How many genes have >= 1 RPKM mean across samples?
+sum(base::rowMeans(ctsSV) >= 1)
+# >= 0.1
+sum(base::rowMeans(ctsSV) >= 0.1)
+# == 0 ?!?
+sum(base::rowMeans(ctsSV) == 0)
 
-head(ctsSV,2)
+#When looking at the data using RPKM, there is some serious skew.
+base::summary(rowMeans(ctsSV))
+
+#Let's plot this: Not a very clear plot initially, 
+#because there are so many zero values in the data, 
+#and it has not been log2 transformed
+ctsSV %>%
+  as.data.frame() %>%
+  rownames_to_column("gene") %>%
+  as_tibble() %>%
+  gather(key = sample, value = count, -gene) %>%
+  inner_join(coldataSV, by = c("sample" = "sample")) %>%
+  ggplot(aes(count, fill = sample, colour = tissue)) +
+  geom_line(stat = "density", adjust = 0.75) +
+  theme_bw()
+
+#remove genes <1 RPKM mean
+ctsSV <- ctsSV[base::rowMeans(ctsSV) >= 1,]
+base::summary(rowMeans(ctsSV))
+
+#Still hard to visualise
+ctsSV %>%
+  as.data.frame() %>%
+  rownames_to_column("gene") %>%
+  as_tibble() %>%
+  gather(key = sample, value = count, -gene) %>%
+  inner_join(coldataSV, by = c("sample" = "sample")) %>%
+  ggplot(aes(count, fill = sample, colour = tissue)) +
+  geom_line(stat = "density", adjust = 0.75) +
+  theme_bw()
+
+#Let's see what log2-transformed data looks like
+ctsSV_log <- log2(ctsSV) #log2(ctsSV + 1) if zero values are present in the matrix
+
+log2(ctsSV) %>%
+  as.data.frame() %>%
+  rownames_to_column("gene") %>%
+  as_tibble() %>%
+  gather(key = sample, value = count, -gene) %>%
+  inner_join(coldataSV, by = c("sample" = "sample")) %>%
+  ggplot(aes(count, fill = sample, colour = tissue)) +
+  geom_line(stat = "density", adjust = 0.75) +
+  theme_bw()
+
+#Why does this matter? Mean vs variance relationship - This is where I am up to
+ctsSV %>%
+  as.data.frame() %>%
+  rownames_to_column("gene") %>%
+  as_tibble() %>%
+  gather(key = sample, value = count, -gene) %>%
+  group_by(gene) %>%
+  mutate(mean_count = mean(count))
+  
+
+ctsSV %>%
+  as.data.frame() %>%
+  rownames_to_column("gene") %>%
+  as_tibble() %>%
+  gather(key = sample, value = count, -gene) %>%
+  inner_join(coldataSV, by = c("sample" = "sample")) %>%
+  ggplot(aes(count, fill = sample, colour = tissue)) +
+  geom_line(stat = "density", adjust = 0.75) +
+  theme_bw() +
+  # scale_color_lancet() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  ylab("Density of Read Counts") +
+  xlab("RPKM") +
+  facet_grid(rows = vars(tissue)) +
+  theme(legend.position = "none") +
+  theme(strip.text.y = element_text(angle = 360)) +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+#We have 65000 'genes' and 12 samples before filtering, 
+#12876 after filtering out genes with less than 1 RPKM mean across samples
+dim(as.matrix(ctsSV)) 
+
+# MetaSV <- file.path("Meta_LCM_hum_VTA_SNC.csv") # Metadata file
+# coldataSV <- read.csv(MetaSV, row.names = 1)
+
+# read_delim to make a 'tidy' tibble
+# Pipe the full coldataSV tibble into a 'select' function, 
+# to just select the columns we want, while also renaming the capitalised names.
+# Arranging the tibble by tissue
+coldataSV <- read_delim("Meta_LCM_hum_VTA_SNC.csv", delim = ",") %>%
+  select("sample" = Run, "cell_type" = Cell_Type, tissue) %>%
+  arrange(tissue)
+
+# coldataSV <- coldataSV[,c("Cell_Type", "tissue")] #[] is for where the data is -- (_rows, _columns)
 
 coldataSV
 
-rownames(coldataSV) <- sub("","", rownames(coldataSV)) 
+head(ctsSV,2)
+
+# rownames(coldataSV) <- sub("","", rownames(coldataSV)) 
 all(rownames(coldataSV) %in% colnames(ctsSV))
 
 all(rownames(coldataSV) == colnames(ctsSV)) # metadata names of runs == run names at header of count matrix
@@ -36,8 +134,12 @@ ddsSV
 ddsSV$tissue <- relevel(ddsSV$tissue, ref = "snc" )
 
 ddsSVDESeq<- DESeq(ddsSV)
-resSV <- results(ddsSVDESeq)
 
+#Find out what this dispersion message is about
+plotDispEsts(ddsSVDESeq)
+
+
+resSV <- results(ddsSVDESeq)
 resSV
 
 resultsNames(ddsSVDESeq)
